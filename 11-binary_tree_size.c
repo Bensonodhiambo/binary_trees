d3067ba4e257fb133d10721973b233094fa59a4b
Bensonodhiambo <benodhiambo.bo@gmail.com>
2024-03-29 20:15:15 -0700

    Task12

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,12 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - Measures the depth of a node in a binary tree.
- * @tree: A pointer to the node to measure the depth.
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: A pointer to the root node of the tree to measure the size of.
  *
- * Return: If tree is NULL, your function must return 0, else return the depth.
+ * Return: The size of the tree.
  */
-size_t binary_tree_depth(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	size_t size = 0;
+
+	if (tree)
+	{
+		size += 1;
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+	}
+	return (size);
 }
